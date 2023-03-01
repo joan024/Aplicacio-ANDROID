c5a865130c0930db05aacb6f41c6d6908991214c
Julss799 <juliavazmar@gmail.com>
2023-03-01 18:42:53 +0100

    Treball del dia 01/03/2023

--- app/src/main/java/com/example/tappingandroid/Inici.java
+++ app/src/main/java/com/example/tappingandroid/Inici.java
@@ -6,11 +6,16 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.icu.text.CaseMap;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SearchView;
 
 public class Inici extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private ImageView logoImatge;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +23,33 @@ public class Inici extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(findViewById(R.id.toolbar));
+        this.setTitle("");
 
         drawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        //SEARCH VIEW
+        SearchView searchView = findViewById(R.id.sv_busqueda);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String filtre) {
+                // Aquí se ejecuta la búsqueda cuando se pulsa Enter
+                // Enviamos los resultados a la siguiente actividad
+                Intent intent = new Intent(Inici.this, Resultats.class);
+                intent.putExtra("filtre", filtre);
+                startActivity(intent);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Aquí se ejecuta la búsqueda en tiempo real mientras se va escribiendo
+                return false;
+            }
+        });
     }
 
     @Override
