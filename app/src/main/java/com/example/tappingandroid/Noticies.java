8246ea45e798e611dc15d826ad723fe51f82a98d
Julss799 <juliavazmar@gmail.com>
2023-04-24 16:33:41 +0200

    Correccions de codi

--- app/src/main/java/com/example/tappingandroid/Noticies.java
+++ app/src/main/java/com/example/tappingandroid/Noticies.java
@@ -78,11 +78,7 @@ public class Noticies extends AppCompatActivity {
         String dataActualString = dateFormat.format(dataActual);
 
         // Construir la consulta SQL
-        String sql = "SELECT * FROM noticia " +
-                "WHERE data_inici <= "+dataActual+" AND (data_fi >= "+dataActual+" OR data_fi = '0000-00-00');";
-        /*String sql = "SELECT * FROM noticia " +
-                "WHERE data_inici BETWEEN '" + dataActualString + "' " +
-                "AND IFNULL(data_fi, '" + dataActualString + "') OR data_fi IS NULL";*/
+        String sql = "SELECT * FROM noticia WHERE data_inici <= '"+dataActualString+"' AND (data_fi >= '"+dataActualString+"' OR data_fi IS NULL)";
 
         conexio = ConexioBD.CONN();
         try {
